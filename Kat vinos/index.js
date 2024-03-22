3d194e7263ec4d5d04f31ed15b4ce15f4cf4dc72
KatyaSan02 <164129191+KatyaSan02@users.noreply.github.com>
2024-03-22 10:00:24 -0600

    Add files via upload

---
+++ Kat vinos/index.js
@@ -0,0 +1,11 @@
+var negativo = "https://www.aamexico.org.mx/"
+
+if (window.confirm("¿Eres mayor de edad?") == false) {
+    cerrar = window.close("prueba1.html");
+    window.open(negativo) + cerrar;
+  }
+
+
+let DerechosAutor = "Sandoval Gonzales Katya Daniela  /  Todos los derechos son reservados. 2024";
+
+console.log(DerechosAutor);
